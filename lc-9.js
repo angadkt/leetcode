813ed21ed29f9d930ac9f2bbb72798d0519c1cfd
angadkt <angadktofficial@gmail.com>
2024-08-21 11:18:37 +0530

    palindrome number

---
+++ lc-9.js
@@ -0,0 +1,11 @@
+var isPalindrome = function(x) {
+    let arr = x.toString();
+    let rev = arr.split("").reverse().join("");
+    let res ;
+    if(arr === rev){
+        res = true;
+    }else{
+        res = false;
+    }
+    return res;
+};
