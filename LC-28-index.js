cfa847cd59821a9717b3b6ef63e4aa913f3f1c3a
angadkt <angadktofficial@gmail.com>
2024-08-24 21:31:08 +0530

    index of first occurence

---
+++ LC-28-index.js
@@ -0,0 +1,7 @@
+var strStr = function(haystack, needle) {
+    let index = haystack.indexOf(needle);
+    return index;
+};
+
+console.log(strStr("sadbutsad","sad"));
+console.log(strStr("leetcode","leeto"));
