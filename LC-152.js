7db0fda7f90b6041ea3614e842b14671103fa87d
angadkt <angadktofficial@gmail.com>
2024-09-08 13:24:04 +0530

    max product of subarray

---
+++ LC-152.js
@@ -0,0 +1,14 @@
+var maxProduct = function(nums) {
+    let res = -Infinity;
+    for (let i = 0; i < nums.length; i++) {
+        let product = 1;
+        for (let j = i; j < nums.length; j++) {
+            product *= nums[j];
+            res = Math.max(res, product);
+        }
+        
+    }
+    return res
+};
+
+console.log(maxProduct([2,3,-2,4]));
