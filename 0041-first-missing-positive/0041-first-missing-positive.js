78d28ac0c2a3fb0c4b0ae95df03fd0e3d28f0ee6
angadkt <162672340+angadkt@users.noreply.github.com>
2024-11-20 09:53:00 +0530

    Time: 77 ms (5.01%), Space: 77.3 MB (5.11%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var firstMissingPositive = function(nums) {
+    let res = nums.sort((a,b)=>a-b)
+    res = [ ...new Set(res)]
+    res = res.filter((item) => item > 0)
+    // console.log(res)
+    let min = Math.min(...res)
+    console.log(min)
+    if(min != 1){
+        return 1
+    }
+    for(let i=0; i<res.length; i++){
+        if(res[i+1] - res[i] != 1){
+            return res[i] + 1
+            break;
+        }
+    }
+    
+};
