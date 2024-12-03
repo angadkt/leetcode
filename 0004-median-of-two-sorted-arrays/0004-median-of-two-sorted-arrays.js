4b86cc2c9792d63ccd4c7b54d98d92b0f1ff9fc2
angadkt <162672340+angadkt@users.noreply.github.com>
2024-12-03 18:06:51 +0530

    Time: 87 ms (5.05%), Space: 58.8 MB (7.58%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number}
+ */
+var findMedianSortedArrays = function(nums1, nums2) {
+    let res = [...nums1, ...nums2]
+    res = res.sort((a,b)=> a-b)
+    console.log(res)
+    let index = 0
+    let result = 0
+    let l = res.length
+    if(l % 2 != 0){
+        index = Math.ceil(l/2)-1
+        console.log(index)
+        result = res[index]
+        return parseFloat(result)
+    }else{
+        index = Math.floor(l/2)-1
+        console.log(index)
+        result = (res[index] + res[index+1])/2
+        return parseFloat(result)
+    }
+};
