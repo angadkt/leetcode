0426857d02744422f9043ac242023aced6f13b6f
angadkt <162672340+angadkt@users.noreply.github.com>
2024-12-14 10:32:33 +0530

    Time: 3 ms (45.7%), Space: 54.2 MB (24.9%) - LeetHub

---
+++ 2812-find-the-maximum-achievable-number/2812-find-the-maximum-achievable-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} num
+ * @param {number} t
+ * @return {number}
+ */
+var theMaximumAchievableX = function(num, t) {
+    // let number = 
+    // for (let i=0; i<=t; i++){
+        
+    // }
+    return num + (t+t)
+};
