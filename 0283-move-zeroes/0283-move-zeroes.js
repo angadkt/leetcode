28e5ee8b0ed2d82162c1779f9a87441db19826f9
angadkt <162672340+angadkt@users.noreply.github.com>
2025-02-28 21:11:21 +0530

    Time: 5 ms (33.74%), Space: 60.7 MB (6.38%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -4,16 +4,16 @@
  */
 var moveZeroes = function(nums) {
     //this method is working
-    // let j = 0;
-    // for(let i=0; i<nums.length; i++){
-    //     if(nums[i] !== 0){
-    //         [nums[i], nums[j]] = [nums[j] , nums[i]]
-    //         j++
-    //     }
-    // }
-    // return nums
+    let j = 0;
+    for(let i=0; i<nums.length; i++){
+        if(nums[i] !== 0){
+            [nums[i], nums[j]] = [nums[j] , nums[i]]
+            j++
+        }
+    }
+    return nums
 
 
     //i have another method
-    return nums.sort((a,b)=> a===0? 1:b===0? -1 : 0 )
+    // return nums.sort((a,b)=> a===0? 1:b===0? -1 : 0 )
 };
